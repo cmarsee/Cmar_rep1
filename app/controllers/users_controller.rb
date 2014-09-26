298f821ffffcac97f100efabab8279752b4e832b
Caitlin Marsee <caitlin_marsee@taylor.edu>
2014-09-26 01:08:32 +0000

    Commiting editing users and refactoring up to Handle trying to edit non-existent user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+	
 	def new
 		@user = User.new
 	end
+	
 	def create
-		@user = User.new(params.require(:user).permit(:name, :email, :password) )
+    @user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the site, #{@user.name}"
 			redirect_to @user
@@ -15,10 +17,31 @@ class UsersController < ApplicationController
 			render "new"
 		end
 	end
+	
 	def show
 		@user = User.find(params[:id])
 	rescue
 		flash[:danger] = "Unable to find user"
 		redirect_to users_path
 	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+    if @user.update(user_params)
+			flash[:success] = "You have modified your profile"
+			redirect_to @user
+		else
+			flash[:danger] = "Unable to update your profile"
+			render 'edit'
+		end
+	end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
